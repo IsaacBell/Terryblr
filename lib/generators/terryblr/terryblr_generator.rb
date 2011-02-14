afad34bfe1714bba756801d06242dedb4ec7e43f
Samuel Mendes <samuel.mendes@gmail.com>
2011-02-14 16:22:47 +0100

    Remove devise dependency, add resource-controller plugin dependency

--- lib/generators/terryblr/terryblr_generator.rb
+++ lib/generators/terryblr/terryblr_generator.rb
@@ -1,21 +1,35 @@
-require 'rails/generators/active_record'
+require 'rails/generators'
+require 'rails/generators/migration'
+require 'rails/generators/active_record/migration'
+
 
 module Terryblr
   module Generators
-    class TerryblrGenerator < ActiveRecord::Generators::Base
+    class TerryblrGenerator < Rails::Generators::Base
       namespace "terryblr"
+      include Rails::Generators::Migration
+      
 
       def self.source_root
         @source_root ||= File.join(File.dirname(__FILE__), 'templates')
       end
 
-      def run_devise_install
-        generate("devise:install") 
-        generate(:devise, name)
+      # Implement the required interface for Rails::Generators::Migration.
+      def self.next_migration_number(dirname)
+        next_migration_number = current_migration_number(dirname) + 1
+        if ActiveRecord::Base.timestamped_migrations
+          [Time.now.utc.strftime("%Y%m%d%H%M%S"), "%.14d" % next_migration_number].max
+        else
+          "%.3d" % next_migration_number
+        end
+      end
+
+      def install_dependencies
+        plugin 'resource_controller', :git => 'git://github.com/anupamc/resource_controller.git'
       end
 
       def create_migration_file
-        migration_template 'migration.rb', "db/migrate/add_is_admin_to_#{table_name}.rb"
+        migration_template 'create_pages.rb', "db/migrate/create_pages.rb"
       end
     end
   end
