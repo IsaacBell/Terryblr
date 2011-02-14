8e1cccf44acd3a391f9e7548d97da3141545abae
Samuel Mendes <samuel.mendes@gmail.com>
2011-02-14 16:21:24 +0100

    Remove fake resource Account

--- lib/generators/terryblr/templates/migration.rb
+++
@@ -1,9 +0,0 @@
-class AddIsAdminTo<%= table_name.camelize %> < ActiveRecord::Migration
-  def self.up
-    add_column :<%= table_name %>, :is_admin, :boolean, :default => false, :null => false
-  end
-
-  def self.down
-    remove_column :<%= table_name %>, :is_admin
-  end
-end
