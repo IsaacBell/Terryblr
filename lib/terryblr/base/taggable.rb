15564b3016203b5c5d76cb9b9d9d552e5b13e709
Samuel Mendes <samuel.mendes@gmail.com>
2011-02-17 16:54:45 +0100

    Oops

---
+++ lib/terryblr/base/taggable.rb
@@ -0,0 +1,25 @@
+module Terryblr
+  class Base < ActiveRecord::Base
+    module Taggable
+
+      def self.included(recipient)
+        recipient.class_eval do
+          # Throws errors if table doesn't exist on first project setup
+          if recipient.table_exists?
+            acts_as_taggable
+            acts_as_taggable_on Terryblr::Settings.tags[self.table_name]['groups'] if defined?(Terryblr::Settings.tags[self.table_name]['groups'])
+            named_scope :tagged, lambda { |tags|
+              tags_sql = tags.is_a?(Array) ? tags.map{|t|"'#{t}'"}.join(",") : "'#{tags}'"
+              { 
+                :joins => "JOIN taggings ON taggings.taggable_id = #{table_name}.id AND taggings.taggable_type = '#{self.to_s}'", 
+                :group => "#{table_name}.id",
+                :conditions => "taggings.tag_id in (SELECT id from tags where name IN (#{tags_sql}))"
+              }
+            }
+          end
+        end
+      end
+
+    end
+  end
+end
