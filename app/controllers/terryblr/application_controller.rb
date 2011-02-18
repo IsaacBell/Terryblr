3fd7acc9a8076b29bc6e18ce88b2dd69da41afb3
Samuel Mendes <samuel.mendes@gmail.com>
2011-02-18 16:14:22 +0100

    Disable user's related helpers

--- app/controllers/terryblr/application_controller.rb
+++ app/controllers/terryblr/application_controller.rb
@@ -2,39 +2,39 @@ class Terryblr::ApplicationController < ResourceController::Base
 
   include Terryblr::MemcachedSystem
   helper 'terryblr/application'
-  helper_method :current_user_session, :current_user
+  #helper_method :current_user_session, :current_user
 
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   private
 
-  def current_user_session
-    return @current_user_session if defined?(@current_user_session)
-    @current_user_session = UserSession.find
-  end
-  
-  def current_user
-    return @current_user if defined?(@current_user)
-    @current_user = current_user_session && current_user_session.record
-  end
-  
-  def require_user
-    unless current_user
-      store_location
-      flash[:notice] = "You must be logged in to access this page"
-      redirect_to new_user_session_path
-      return false
-    end
-  end
+  #def current_user_session
+  #  return @current_user_session if defined?(@current_user_session)
+  #  @current_user_session = UserSession.find
+  #end
+  #
+  #def current_user
+  #  return @current_user if defined?(@current_user)
+  #  @current_user = current_user_session && current_user_session.record
+  #end
+  #
+  #def require_user
+  #  unless current_user
+  #    store_location
+  #    flash[:notice] = "You must be logged in to access this page"
+  #    redirect_to new_user_session_path
+  #    return false
+  #  end
+  #end
 
-  def require_no_user
-    if current_user
-      store_location
-      flash[:notice] = "You must be logged out to access this page"
-      redirect_to user_path(current_user)
-      return false
-    end
-  end
+  #def require_no_user
+  #  if current_user
+  #    store_location
+  #    flash[:notice] = "You must be logged out to access this page"
+  #    redirect_to user_path(current_user)
+  #    return false
+  #  end
+  #end
   
   def store_location
     session[:return_to] = request.request_uri
