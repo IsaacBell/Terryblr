032737e30c8f05fe6d5ac9d8886256ad0e08e668
Samuel Mendes <samuel.mendes@gmail.com>
2011-02-18 16:13:58 +0100

    Update schema with taggings table

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -94,4 +94,21 @@ ActiveRecord::Schema.define(:version => 20110217173060) do
   add_index "posts", ["twitter_id"], :name => "index_posts_on_twitter_id"
   add_index "posts", ["votes_count"], :name => "index_posts_on_votes_count"
 
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context"
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
+  end
+
 end
