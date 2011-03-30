cbf3562b63dbdadd35aaf351782ff656341f886b
mathieuravaux <mathieu.ravaux@gmail.com>
2011-03-30 18:20:44 +0200

    Fixed RSpec throwing "Stack level too deep" indistinctly

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,7 +37,6 @@ Dir["#{File.dirname(__FILE__)}/factories/**/*.rb"].each { |f| require f }
     # Remove this line if you don't want RSpec's should and should_not
     # methods or matchers
     require 'rspec/expectations'
-    config.include RSpec::Matchers
 
     # == Mock Framework
     config.mock_with :rspec
