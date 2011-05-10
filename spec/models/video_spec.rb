f0a1fbff00771a623eb80a81d3760dd7978a5ac1
Lachlan Laycock <lachy@lovethe88.com>
2011-05-10 15:03:59 +0200

    Removing obsolete video tests

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -5,21 +5,23 @@ describe Terryblr::Video do
     before do
       @post = Factory(:published_post)
     end
-
-    it "should update associated post on create" do
-      video = Factory.build(:video, :post => @post)
-      video.stub(:upload_video).and_return(true)
-      @post.should_receive(:touch)
-      video.save
-    end
-
-    it "should update associated post on update" do
-      video = Factory.build(:video)
-      video.stub(:upload_video).and_return(true)
-      video.save
-      video.post = @post
-      @post.should_receive(:touch)
-      video.save
-    end
+    # 
+    # No longer possible since we're using nested RESTful forms - eg: you can't touch a non-existant object
+    # 
+    # it "should update associated post on create" do
+    #   video = Factory.build(:video, :part => @part)
+    #   video.stub(:upload_video).and_return(true)
+    #   @post.should_receive(:touch)
+    #   video.save
+    # end
+    # 
+    # it "should update associated post on update" do
+    #   video = Factory.build(:video)
+    #   video.stub(:upload_video).and_return(true)
+    #   video.save
+    #   video.post = @post
+    #   @post.should_receive(:touch)
+    #   video.save
+    # end
   end
 end
